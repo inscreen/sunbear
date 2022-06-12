ccd63cddf226576143282b556a1a25a8b4719bbc
Ramon Snir <r@mon.dev>
2022-06-12 01:01:42 +0000

    fix actors queries

--- src/Driver.ts
+++ src/Driver.ts
@@ -1,6 +1,7 @@
 import type { PlannedQuery } from './queryPlan';
 
 export interface Driver<Query, Node> {
-    emit<Goal extends Node>(query: PlannedQuery<Node, Goal>): Query;
+    emitGoalsQuery<Goal extends Node, Actor extends Node>(query: PlannedQuery<Node, Goal, Actor>): Query;
+    emitActorsQuery<Goal extends Node, Actor extends Node>(query: PlannedQuery<Node, Goal, Actor>): Query;
     run<Goal extends Node>(query: Query): Promise<Goal[]>;
 }
--- src/drivers/TypeOrmDriver.ts
+++ src/drivers/TypeOrmDriver.ts
@@ -25,11 +25,36 @@ export class TypeOrmDriver<
         }
     }
 
-    emit<Goal extends Node>(query: PlannedQuery<Node, Goal>): SelectQueryBuilder<Goal> {
+    emitGoalsQuery<Goal extends Node, Actor extends Node>(
+        query: PlannedQuery<Node, Goal, Actor>,
+    ): SelectQueryBuilder<Goal> {
         const goalPrimaryColumn = getNodeDefinition(this.schema, query.alternatives[0]!.goalTable).primaryColumn;
         return this.dataSource
             .createQueryBuilder(query.alternatives[0]!.goalTable, 'goal')
-            .where(`goal.${format.ident(goalPrimaryColumn)} IN (${emitSql(this.schema, query, goalPrimaryColumn)})`);
+            .where(
+                `goal.${format.ident(goalPrimaryColumn)} IN (${emitSql(
+                    this.schema,
+                    query,
+                    'goal',
+                    goalPrimaryColumn,
+                )})`,
+            );
+    }
+
+    emitActorsQuery<Goal extends Node, Actor extends Node>(
+        query: PlannedQuery<Node, Goal, Actor>,
+    ): SelectQueryBuilder<Actor> {
+        const actorPrimaryColumn = getNodeDefinition(this.schema, query.alternatives[0]!.actorTable).primaryColumn;
+        return this.dataSource
+            .createQueryBuilder(query.alternatives[0]!.actorTable, 'actor')
+            .where(
+                `actor.${format.ident(actorPrimaryColumn)} IN (${emitSql(
+                    this.schema,
+                    query,
+                    'actor',
+                    actorPrimaryColumn,
+                )})`,
+            );
     }
 
     async run<Goal extends Node>(query: SelectQueryBuilder<Goal>): Promise<Goal[]> {
